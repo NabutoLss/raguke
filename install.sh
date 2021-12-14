75c1cc5b9873b7d7c3cef9ef90689e6e62c0c489
ninem <78937774+nabutols@users.noreply.github.com>
2021-12-14 10:51:09 -0300

    Initial commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+apt-get update
+apt-get upgrade
+apt-get install -y nodejs libwebp ffmpeg wget tesseract
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm install
+
+echo "O BOT FOI INSTALADO COM SUCESSO"
+#!/usr/bin/bash
+
+clear
+node index.js
---
+++ src/menus/menulista.js
@@ -0,0 +1,100 @@
+const menulista = `
+𝙁𝙄𝙂𝙐𝙍𝙄𝙉𝙃𝘼𝙎
+➙ /𝙨𝙩𝙞𝙘𝙠𝙚𝙧 
+fazer figurinhas
+
+➙ /𝙨𝙩𝙞𝙘𝙠𝙚𝙧𝙣𝙤𝙢𝙚
+colocar seu nome na desc da figu
+
+➙ /𝙛𝙞𝙜𝙪𝙖𝙣𝙞𝙢𝙚
+figurinha de anime
+
+➙ /𝙩𝙤𝙞𝙢𝙜
+figu --> img
+
+𝙂𝙍𝙐𝙋𝙊
+➙ /𝙖𝙙𝙙
+adicionar alguém no grupo
+
+➙ /𝙜𝙧𝙪𝙥𝙤
+fechar ou abrir gp
+
+➙ /𝙡𝙞𝙣𝙠𝙜𝙥
+link do grupo
+
+➙ /𝙧𝙚𝙨𝙚𝙩𝙖𝙧𝙡𝙞𝙣𝙠
+resetar link do grupo
+
+➙ /𝙗𝙖𝙣𝙞𝙧
+banir um participante
+
+➙ /𝙝𝙞𝙙𝙚𝙩𝙖𝙜
+marcar com msg
+
+➙ /𝟭𝟱𝟳
+alterar configurações de grupo
+
+➙ /𝙗𝙖𝙣𝙞𝙧𝟮
+banir participante pela mensagem
+
+➙ /𝙥𝙧𝙤𝙢𝙤𝙫𝙚𝙧
+promover membro para administrador
+
+➙ /𝙧𝙚𝙗𝙖𝙞𝙭𝙖𝙧
+rebaixar administrador
+
+𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿
+➙ /𝙥𝙡𝙖𝙮
+fazer download de msc pelo título
+
+➙ /𝙥𝙡𝙖𝙮2
+baixar musica 2
+
+➙ /𝙩𝙞𝙠𝙩𝙤𝙠
+fazer download vídeo do ttk
+
+
+𝘿𝙊𝙉𝙊
+➙ /𝙖𝙥𝙖𝙜𝙖𝙧𝙘𝙝𝙖𝙧
+limpar todas as conversas
+
+➙ /𝙩𝙢
+fazer transmissão para todos os cntts
+
+
+𝙊𝙐𝙏𝙍𝙊𝙎
+➙ /𝙩𝙧𝙤𝙡𝙡𝙚𝙞
+gif com áudio
+
+➙ /𝙚𝙪𝙣𝙪𝙣𝙘𝙖
+jogo do eu nunca
+
+➙ /𝙘𝙧𝙞𝙖𝙧𝙡𝙤𝙘
+criar localização
+
+
+𝙄𝙈𝘼𝙂𝙀𝙈
+➙ /𝙡𝙤𝙡𝙞
+enviar imagens de loli
+
+
+𝗣𝗘𝗦𝗤𝗨𝗜𝗦𝗔
+➙ /𝙜𝙤𝙤𝙜𝙡𝙚
+
+
+𝗠𝗢𝗗𝗜𝗙𝗜𝗖𝗔𝗗𝗢𝗥 𝗗𝗘 𝗩𝗢𝗭
+➙ /𝙜𝙧𝙤𝙨𝙖
+deixar a voz grossa
+
+➙ /𝙜𝙧𝙖𝙫𝙚
+deixar áudio com grave
+
+➙ /𝙧𝙤𝙗𝙤̂
+deixar voz de robô
+
+`
+
+exports.menulista = menulista
+
+
+
